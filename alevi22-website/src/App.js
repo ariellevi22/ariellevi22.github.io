37a93c7bbe111ca6b6c005b2f6978f245b2a73e0
Ariel Levi <54635906+alevi22@users.noreply.github.com>
2021-01-11 14:09:07 -0600

    Moved logo SVG file

--- alevi22-website/src/App.js
+++ alevi22-website/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import logo from './Logo.svg';
+import logo from './Assets/Logo.svg';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import OverlayCard from './Components/OverlayCard/OverlayCard';
