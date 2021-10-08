10913b9f807239006624524b6c54e98eda69bb56
Ariel Levi <54635906+alevi22@users.noreply.github.com>
2021-10-08 14:12:56 -0500

    Updated about section

--- ClientApp/src/Containers/About.tsx
+++ ClientApp/src/Containers/About.tsx
@@ -18,12 +18,10 @@ const About = () => {
             </p>
 
             <p className={noSpacing}>
-                During summer 2020, I worked at <Link href="https://zoom.us" openWithNewTab>Zoom</Link> as
-                a technical support engineer intern, helping customers achieve success using
-                Zoom’s products during the difficult times of the COVID-19 pandemic.
-                Last summer, I worked as an application developer intern
-                at <Link href="https://evergy.com" openWithNewTab>Evergy</Link>,
-                where I continued to grow my skillset and apply my learning to a real-world environment.
+                Last summer, I worked as an application developer intern at <Link href="https://evergy.com" openWithNewTab>Evergy</Link>,
+                where I created tools and integrations to help employees be more productive at work.
+                Currently, I am looking for front-end or full-stack software engineering internship opportunities
+                for summer 2022 to continue to grow my skillset and apply my learning to a real-world environment.
             </p>
         </Section>
     );
